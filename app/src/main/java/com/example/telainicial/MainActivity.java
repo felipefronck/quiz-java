0ff5c98a1095b2cf4c4b1b500c06052866203c4f
felipefronck <fronckowiakfelipe@gmail.com>
2024-10-02 10:24:42 -0300

    estrutura base, main e primeira pergunta

---
+++ app/src/main/java/com/example/telainicial/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.telainicial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onIniciarQuizBtnClick(View view){
+        Intent proxTela = new Intent(this, Pergunta1.class);
+        startActivity(proxTela);
+    }
+}
